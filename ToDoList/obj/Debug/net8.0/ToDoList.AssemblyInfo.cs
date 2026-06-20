f0a40b2ec3e13fb69a8a8e66a72898f292a46da1
Денис Кибицкий <aidns410@gmail.com>
2026-06-20 21:21:17 +0300

    check some changes

--- ToDoList/obj/Debug/net8.0/ToDoList.AssemblyInfo.cs
+++ ToDoList/obj/Debug/net8.0/ToDoList.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38a215f5977fb22a6034d3501d9998a66e0ee8ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c493a4b80d3b85125b950e8f312dad608acde5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

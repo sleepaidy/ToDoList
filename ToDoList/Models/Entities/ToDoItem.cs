c8cd0d481571f1ef7d5836b5ce0f698b3e402cc8
Денис Кибицкий <aidns410@gmail.com>
2026-06-28 10:57:07 +0300

    add entity and status enum

---
+++ ToDoList/Models/Entities/ToDoItem.cs
@@ -0,0 +1,15 @@
+﻿using ToDoList.Enums;
+
+namespace ToDoList.Models.Entities
+{
+    public class ToDoItem
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime? DeadlineAt { get; set; }
+        public Priority Priority { get; set; }
+        public string Caregory { get; set; }
+        public bool IsImportant { get; set; }
+        public Status Status { get; set; }
+    }
+}

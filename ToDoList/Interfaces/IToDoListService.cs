aebc9387c42455ad2124519c85b9154cde416367
Денис Кибицкий <aidns410@gmail.com>
2026-06-28 11:18:12 +0300

    add interface

---
+++ ToDoList/Interfaces/IToDoListService.cs
@@ -0,0 +1,10 @@
+﻿using ToDoList.Models.Entities;
+using ToDoList.Models.Home;
+
+namespace ToDoList.Interfaces
+{
+    public interface IToDoListService
+    {
+        ToDoItem CreateTusk(ToDoTuskViewModel viewModel);
+    }
+}

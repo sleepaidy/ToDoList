9515f8476d69a79d8d7316bf6169d13a71c10af4
Денис Кибицкий <aidns410@gmail.com>
2026-06-28 11:18:12 +0300

    add interface

--- ToDoList/Models/Entities/ToDoItem.cs
+++ ToDoList/Models/Entities/ToDoItem.cs
@@ -4,12 +4,15 @@ namespace ToDoList.Models.Entities
 {
     public class ToDoItem
     {
+        public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public DateTime? DeadlineAt { get; set; }
+        public DateTime CreateAt { get; set; }
         public Priority Priority { get; set; }
-        public string Caregory { get; set; }
+        public string Category { get; set; }
         public bool IsImportant { get; set; }
         public Status Status { get; set; }
+
     }
 }
--- ToDoList/Services/ToDoListServices.cs
+++ ToDoList/Services/ToDoListServices.cs
@@ -1,7 +1,15 @@
-﻿namespace ToDoList.Services
+﻿using ToDoList.Models.Entities;
+
+namespace ToDoList.Services
 {
     public class ToDoListServices
     {
-        
+        private readonly List<ToDoItem> _tasks = new();
+        private int _nextId = 1;
+
+        public List<ToDoItem> CreateTask
+        {
+
+        }
     }
 }

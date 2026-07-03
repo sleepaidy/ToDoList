1b8e3afb3b3208824d3c3ff3339e427e4ee4a42d
Денис Кибицкий <aidns410@gmail.com>
2026-07-03 20:38:35 +0300

    add UserRepository

---
+++ ToDoList.Data/Models/UserData.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDoList.Data.Models
+{
+    public class UserData
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string ProfileImage {  get; set; }
+    }
+}
